7a3db305ad69d4a45e33925ce16542a30bf73b78
Augustin C <augustin.darkangel@gmail.com>
2016-10-08 15:14:33 +0300

    Added admin panel options

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,14 +1,51 @@
+var Movie = require('../models/Movie.model');
+
 var adminController = function() {
 
 	var getIndex = function(req, res) {
+		if(req.isAuthenticated()) return res.redirect('/admin/profile')
 		res.render('admin/index', {
 			user: req.user,
 			csrfToken: req.csrfToken()
 		});
 	};
 
+	var getProfile = function(req, res) {
+		Movie.find({}, function(err, movies) {
+			if(err) throw err;
+
+			res.render('admin/profile', {
+				user: req.user,
+				movies: movies
+			});
+		});
+	};
+
+	var getAdd = function(req, res) {
+		res.render('admin/add', {
+			user: req.user,
+			csrfToken: req.csrfToken()
+		});
+	};
+
+	var postAdd = function(req, res) {
+		var newMovie = new Movie();
+		newMovie.title = req.body.title;
+		newMovie.description = req.body.description;
+		newMovie.cast = req.body.cast;
+		newMovie.img = req.body.image;
+
+		newMovie.save(function(err, movie) {
+			if(err) throw err;
+			res.redirect('/admin/profile');
+		});
+	};
+
 	return {
-		getIndex: getIndex
+		getIndex: getIndex,
+		getProfile: getProfile,
+		getAdd: getAdd,
+		postAdd: postAdd
 	}
 
 };
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,6 +2,18 @@ var express = require('express'),
 	adminRoute = express.Router(),
 	adminController = require('../controllers/adminController')();
 
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+
+	res.redirect('/');
+}
+
 adminRoute.route('/').get(adminController.getIndex);
+adminRoute.route('/profile').get(isLoggedIn, adminController.getProfile);
+adminRoute.route('/add')
+		  .get(isLoggedIn, adminController.getAdd)
+		  .post(isLoggedIn, adminController.postAdd);
 
 module.exports = adminRoute;
